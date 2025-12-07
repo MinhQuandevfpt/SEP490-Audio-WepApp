6bb30528f8b4d06317494f673b9e7a26e0231d8a
MinhQuandevfpt <quankontum55555@gmail.com>
2025-12-07 13:36:21 +0700

    vite3

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
   server: {
     port: 5173,
-    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
-  build: {
-    sourcemap: false, // giảm size, tăng tốc
-    chunkSizeWarningLimit: 1000, // tránh cảnh báo dư thừa
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        // Tách vendor an toàn – Vercel recommended
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // Tách các thư viện lớn ra riêng để tối ưu
-            if (id.includes("react") || id.includes("react-dom")) {
-              return "vendor-react";
-            }
-            if (id.includes("firebase")) {
-              return "vendor-firebase";
-            }
-            if (id.includes("three") || id.includes("@react-three")) {
-              return "vendor-three";
-            }
-            return "vendor"; // Gom các lib còn lại vào vendor
-          }
-        },
-      },
-    },
-  },
-
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
